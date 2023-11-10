9d130052dfe433df560bac4605d4c91cf2b473f3
AndresAlvarado29 <nalvaradoc1@est.ups.edu.ec>
2023-11-10 16:22:47 -0500

    cambios en la estructura del programa y uso de ejemplo de jms y apache active

--- src/main/java/ec/edu/ups/pracicaSocket/servidor/Servidor.java
+++ src/main/java/ec/edu/ups/pracicaSocket/servidor/Servidor.java
@@ -10,8 +10,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Observable;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  *
